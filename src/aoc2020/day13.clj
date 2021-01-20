4e3fffde208387fd40fea9d336320d1978828ca3
Radagast <r.slabon@gmail.com>
2021-01-21 00:25:24 +0100

    WIP - day 13 part 2

--- src/aoc2020/day13.clj
+++ src/aoc2020/day13.clj
@@ -13,8 +13,8 @@
 (defn find-bus-id-and-delay
   [timestamp bus-ids]
   (let [[bus-timestamp bus-id] (first (sort (map
-                                                (fn [bus-id] [(multiple-or-greater bus-id timestamp) bus-id])
-                                                bus-ids)))
+                                              (fn [bus-id] [(multiple-or-greater bus-id timestamp) bus-id])
+                                              bus-ids)))
         delay (- bus-timestamp timestamp)]
     [bus-id delay]))
 
@@ -32,3 +32,28 @@
   (let [[timestamp bus-ids] (parse input)
         [bus-id delay] (find-bus-id-and-delay timestamp bus-ids)]
     (* bus-id delay)))
+
+(defn found-timestamp?
+  [timestamp timetable]
+  (let [timestamps (mapv (fn [[bus-id offset]] (mod (+ timestamp offset) bus-id)) timetable)]
+    (every? #(= 0 %) timestamps)
+    ))
+
+(defn parse-2
+  [input]
+  (let [[_ bus-ids] (str/split-lines input)
+        bus-ids (str/split bus-ids #",")
+        bus-ids (map-indexed (fn [idx bus-id] [bus-id idx]) bus-ids)
+        bus-ids (filter (fn [[bus-id _]] (not= "x" bus-id)) bus-ids)
+        bus-ids (mapv (fn [[bus-id idx]] [(Long/parseLong bus-id) idx]) bus-ids)]
+    bus-ids))
+
+(defn solve-2
+  [timetable]
+  (let [[bus-id offset] (last (sort timetable))]
+    (loop [idx 1]
+      (if (found-timestamp? (- (* bus-id idx) offset) timetable)
+        (- (* bus-id idx) offset)
+        (recur (inc idx))
+        )
+      )))
--- test/aoc2020/day13_test.clj
+++ test/aoc2020/day13_test.clj
@@ -16,3 +16,27 @@
     (is (= 3789 (solve-1 (slurp (io/resource "day13.txt")))))
 
     ))
+
+(deftest found-timestamp?-test
+  (testing "found-timestamp?"
+
+    (is (= true (found-timestamp? 1068781 [[7 0],[13 1],[59 4],[31 6],[19 7]])))
+
+    ))
+
+(deftest solve-2-example
+  (testing "solve-2-example"
+
+    (is (= 1068781 (solve-2 [[7 0],[13 1],[59 4],[31 6],[19 7]])))
+    (is (= 3417 (solve-2 [[17 0],[13 2],[19 3]])))
+    (is (= 1202161486 (solve-2 [[1789 0],[37 1],[47 2][1889 3]])))
+    (is (= 1261476 (solve-2 [[67 0],[7 1],[59 3][61 4]])))
+
+    ))
+
+(deftest solve-2-input
+  (testing "solve-2-input"
+
+    (is (= 0 (solve-2 (parse-2 (slurp (io/resource "day13.txt"))))))
+
+    ))
