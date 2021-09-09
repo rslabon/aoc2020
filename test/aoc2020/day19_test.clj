d6d45b8c7287c912bec5c3d4dd4902e3801738a1
Radagast <r.slabon@gmail.com>
2021-09-09 23:14:53 +0200

    day 19 - in progress

--- test/aoc2020/day19_test.clj
+++ test/aoc2020/day19_test.clj
@@ -11,39 +11,43 @@
         _ (println text-idx)
         ]
     (cond
-      (char? rule-body) (= rule-body (get text text-idx))
+      (char? rule-body) (if (= rule-body (get text text-idx)) 1 0)
       (number? rule-body) (count-matched text text-idx rules-def rule-body)
-      ;(empty? rule-body) 0
-      (coll? (first rule-body)) (let [[sub-rule-1 sub-rule-2] rule-body
-                                      ;_ (println sub-rule-1 sub-rule-2)
+      :else (let [[sub-rule-1 sub-rule-2] rule-body
                                       match-sub-rule-1 (map-indexed (fn [idx item] (count-matched text (+ idx text-idx) rules-def item)) sub-rule-1)
-                                      match-sub-rule-2  (map-indexed (fn [idx item] (count-matched text (+ idx text-idx) rules-def item)) sub-rule-2)
+                                      match-sub-rule-2 (map-indexed (fn [idx item] (count-matched text (+ idx text-idx) rules-def item)) sub-rule-2)
                                       _ (print match-sub-rule-1 match-sub-rule-2)
                                       ]
-                                  (or (= #{true} (set match-sub-rule-1)) (= #{true} (set match-sub-rule-2)))
+                                  (max (reduce + match-sub-rule-1) (reduce + match-sub-rule-2))
                                   ))))
 
+(defn valid?
+  [text text-idx rules-def rule-idx]
+  (= (count text) (count-matched text text-idx rules-def rule-idx))
+  )
 
-(deftest count-matched-test
-  (testing "count-matched"
-    ;(is (= true (count-matched "a" 0 {0 \a} 0)))
-    ;(is (= false (count-matched "b" 0 {0 \a} 0)))
-    ;(is (= true (count-matched "ab" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
-    ;(is (= false (count-matched "cc" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
-    ;(is (= false (count-matched "aa" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
-    ;(is (= true (count-matched "ab" 0 {0 [[] [1 2]], 1 \a, 2 \b} 0)))
-    ;(is (= false (count-matched "c" 0 {0 [[1 2] []], 1 \a, 2 \b, 3 \c} 0)))
-    (is (= false (count-matched "ab" 0 {0 [[1] []], 1 \a, 2 \b} 0)))
+(deftest valid?-test
+  (testing "valid?"
+    ;(is (= true (valid? "a" 0 {0 \a} 0)))
+    ;(is (= false (valid? "b" 0 {0 \a} 0)))
+    ;(is (= true (valid? "ab" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
+    ;(is (= false (valid? "cc" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
+    ;(is (= false (valid? "aa" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
+    ;(is (= true (valid? "ab" 0 {0 [[] [1 2]], 1 \a, 2 \b} 0)))
+    ;(is (= false (valid? "c" 0 {0 [[1 2] []], 1 \a, 2 \b, 3 \c} 0)))
+    ;(is (= false (valid? "ab" 0 {0 [[1] []], 1 \a, 2 \b} 0)))
 
-    ;(is (= true (count-matched "abc" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [] ], 3 \b, 4 \c} 0)))
-    ;(is (= true (count-matched "acb" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
-    ;(is (= false (count-matched "cba" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
-    ;(is (= false (count-matched "cba" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
-    ;(is (= true (count-matched "aa" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [5]], 3 \b, 4 \c, 5 [[1] []]} 0)))
-    ;(is (= false (count-matched "ac" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [5]], 3 \b, 4 \c, 5 [[1] []]} 0)))
+    ;(is (= true (valid? "abc" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [] ], 3 \b, 4 \c} 0)))
+    ;(is (= true (valid? "acb" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
+    ;(is (= false (valid? "cba" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
+    ;(is (= false (valid? "cba" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
+    ;(is (= true (valid? "aa" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [5]], 3 \b, 4 \c, 5 [[1] []]} 0)))
+    ;(is (= false (valid? "ac" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [5]], 3 \b, 4 \c, 5 [[1] []]} 0)))
+    ;(is (= true (valid? "abc" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [5]], 3 \b, 4 \c, 5 [[1] []]} 0)))
 
 
-    ;(is (= true (count-matched "ababbb" 0 rules-def 0)))
+    ;(is (= true (valid? "ababbb" 0 rules-def 0)))
+    (is (= 1 (count-matched "ababbb" 0 rules-def 0)))
     ))
 
 ;0: 4 1 5
