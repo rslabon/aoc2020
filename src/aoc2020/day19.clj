5771dd553027be6468e3814409236bc979c32357
Radagast <r.slabon@gmail.com>
2021-09-10 18:47:03 +0200

    day 19 - part 1

--- src/aoc2020/day19.clj
+++ src/aoc2020/day19.clj
@@ -1,1 +1,42 @@
-(ns aoc2020.day19)
+(ns aoc2020.day19
+  (:require [clojure.string :as str]))
+
+(defn parse-rule-def-line
+  [line]
+  (let [single-char (re-matches #"(\d+): \"(\w)\"" line)
+        nested-rules (re-matches #"(\d+): ((\s*\d+)+)(\s\|((\s\d+)+))?" line)]
+    (cond (not= nil single-char) (let [[_ idx char] single-char
+                                       idx (read-string idx)
+                                       char (str/replace char "\"" "")
+                                       [char] char]
+                                   {idx char})
+          (not= nil nested-rules) (let [[_ idx rules _ _ sub-rules] nested-rules
+                                        idx (read-string idx)
+                                        rules (str/split rules #" ")
+                                        rules (mapv read-string rules)
+                                        sub-rules (if (nil? sub-rules)
+                                                    []
+                                                    (mapv read-string (-> sub-rules
+                                                                          (str/trim)
+                                                                          (str/split #" ")
+                                                                          )))]
+                                    {idx [rules sub-rules]}))))
+
+(defn parse-rule-def
+  [input]
+  (let [lines (str/split-lines input)]
+    (reduce (fn [acc line] (merge acc (parse-rule-def-line line))) {} lines)
+    ))
+
+(defn gen
+  ([rules-def rules] (filter not-empty (gen rules-def rules "")))
+  ([rules-def rules text]
+   (if (and (not-empty rules) (coll? (first rules)))
+     (flatten (map #(gen rules-def % text) rules))
+     (reduce (fn [acc val]
+               (let [rule-body (get rules-def val)]
+                 (if (char? rule-body)
+                   (map #(str % rule-body) (flatten [acc]))
+                   (gen rules-def rule-body acc)
+                   ))
+               ) text rules))))
--- test/aoc2020/day19_test.clj
+++ test/aoc2020/day19_test.clj
@@ -1,54 +1,10 @@
 (ns aoc2020.day19-test
   (:require [clojure.test :refer :all])
-  (:require [aoc2020.day19 :refer :all]))
+  (:require [aoc2020.day19 :refer :all]
+            [clojure.string :as str]
+            [clojure.java.io :as io]))
 
-
-(def rules-def {0 [[4 1 5] []], 1 [[2 3] [3 2]], 2 [[4 4] [5 5]], 3 [[4 5] [5 4]], 4 \a, 5 \b})
-
-(defn count-matched
-  [text text-idx rules-def rule-idx]
-  (let [rule-body (get rules-def rule-idx)
-        _ (println text-idx)
-        ]
-    (cond
-      (char? rule-body) (if (= rule-body (get text text-idx)) 1 0)
-      (number? rule-body) (count-matched text text-idx rules-def rule-body)
-      :else (let [[sub-rule-1 sub-rule-2] rule-body
-                                      match-sub-rule-1 (map-indexed (fn [idx item] (count-matched text (+ idx text-idx) rules-def item)) sub-rule-1)
-                                      match-sub-rule-2 (map-indexed (fn [idx item] (count-matched text (+ idx text-idx) rules-def item)) sub-rule-2)
-                                      _ (print match-sub-rule-1 match-sub-rule-2)
-                                      ]
-                                  (max (reduce + match-sub-rule-1) (reduce + match-sub-rule-2))
-                                  ))))
-
-(defn valid?
-  [text text-idx rules-def rule-idx]
-  (= (count text) (count-matched text text-idx rules-def rule-idx))
-  )
-
-(deftest valid?-test
-  (testing "valid?"
-    ;(is (= true (valid? "a" 0 {0 \a} 0)))
-    ;(is (= false (valid? "b" 0 {0 \a} 0)))
-    ;(is (= true (valid? "ab" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
-    ;(is (= false (valid? "cc" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
-    ;(is (= false (valid? "aa" 0 {0 [[1 2] []], 1 \a, 2 \b} 0)))
-    ;(is (= true (valid? "ab" 0 {0 [[] [1 2]], 1 \a, 2 \b} 0)))
-    ;(is (= false (valid? "c" 0 {0 [[1 2] []], 1 \a, 2 \b, 3 \c} 0)))
-    ;(is (= false (valid? "ab" 0 {0 [[1] []], 1 \a, 2 \b} 0)))
-
-    ;(is (= true (valid? "abc" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [] ], 3 \b, 4 \c} 0)))
-    ;(is (= true (valid? "acb" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
-    ;(is (= false (valid? "cba" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
-    ;(is (= false (valid? "cba" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [4 3]], 3 \b, 4 \c} 0)))
-    ;(is (= true (valid? "aa" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [5]], 3 \b, 4 \c, 5 [[1] []]} 0)))
-    ;(is (= false (valid? "ac" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [5]], 3 \b, 4 \c, 5 [[1] []]} 0)))
-    ;(is (= true (valid? "abc" 0 {0 [[1 2] []], 1 \a, 2 [[3 4] [5]], 3 \b, 4 \c, 5 [[1] []]} 0)))
-
-
-    ;(is (= true (valid? "ababbb" 0 rules-def 0)))
-    (is (= 1 (count-matched "ababbb" 0 rules-def 0)))
-    ))
+(def puzzle-input (slurp (io/resource "day19.txt")))
 
 ;0: 4 1 5
 ;1: 2 3 | 3 2
@@ -57,11 +13,59 @@
 ;4: "a"
 ;5: "b"
 
-;a aa ab b
-
 ;ababbb
 ;bababa
 ;abbbab
 ;aaabbb
 ;aaaabbb
 
+(def rules-def {0 [[4 1 5] []], 1 [[2 3] [3 2]], 2 [[4 4] [5 5]], 3 [[4 5] [5 4]], 4 \a, 5 \b})
+
+(deftest gen-test
+  (testing "gen"
+    (is (= ["aaa"] (gen {1 \a} [1 1 1])))
+    (is (= ["aaa" "bbb"] (gen {1 \a, 2 \b} [[1 1 1] [2 2 2]])))
+    (is (= ["aabb"] (gen {1 \a, 2 \b, 3 [[1 2]]} [[1 3 2]])))
+    (is (= (set ["aaaabb" "aaabab" "abbabb" "abbbab" "aabaab" "aabbbb" "abaaab" "ababbb"]) (set (gen rules-def [0]))))
+    ))
+
+(defn valid?
+  [text rules-def]
+  (let [possible-matches (set (gen rules-def [0]))]
+    (contains? possible-matches text)
+    ))
+
+(deftest valid?-test
+  (testing "valid?"
+    (is (= true (valid? "ababbb" rules-def)))
+    (is (= true (valid? "abbbab" rules-def)))
+    (is (= false (valid? "bababa" rules-def)))
+    (is (= false (valid? "aaabbb" rules-def)))
+    (is (= false (valid? "aaaabbb" rules-def)))
+    ))
+
+(def input "2: 4 4 | 5 5\n4: \"a\"\n0: 4 1 5\n1: 2 3 | 3 2\n3: 4 5 | 5 4\n5: \"b\"\n0: 4 1 5")
+
+(deftest parse-rule-def-test
+  (testing "parse-rule-def"
+    (is (= {0 \a} (parse-rule-def "0: \"a\"")))
+    (is (= {1 [[2 3] []]} (parse-rule-def "1: 2 3")))
+    (is (= {1 [[2 3] [4 5]]} (parse-rule-def "1: 2 3 | 4 5")))
+    (is (= rules-def (parse-rule-def input)))
+    ))
+
+(defn solve-1
+  [input]
+  (let [[rules-def-input messages] (str/split input #"\n\n")
+        rules-def (parse-rule-def rules-def-input)
+        messages (str/split-lines messages)
+        possible-matches (set (gen rules-def [0]))
+        matched (map #(contains? possible-matches %) messages)
+        ]
+    (count (filter true? matched))
+    ))
+
+(deftest solve-1-test
+  (testing "solve-1"
+    (is (= 113 (solve-1 puzzle-input)))
+    ))
