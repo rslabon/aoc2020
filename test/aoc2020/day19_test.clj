17d77857846ae6b62d7e44287213a94538d0da70
Radagast <r.slabon@gmail.com>
2021-09-16 23:07:26 +0200

    day 19 - part 2 done

--- test/aoc2020/day19_test.clj
+++ test/aoc2020/day19_test.clj
@@ -28,18 +28,31 @@
      ""
      (let [rule (first rules)
            rule-body (get rules-def rule)
-           _ (println "xxx" rule rule-body)]
-       (str (if (char? rule-body) (str rule-body)
-                                  (if (coll? (first rule-body))
-                                    (let [sub-rules (first rule-body)
-                                          sub-rules2 (second rule-body)
-                                          r1 (to-regex rules-def sub-rules)
-                                          r2 (to-regex rules-def sub-rules2)
-                                          r (if (empty? r2) r1 (str "(" r1 "|" r2 ")"))]
-                                      r)
-                                    (reduce (fn [acc other-rule] (str acc (to-regex rules-def [other-rule]))) "" rule-body)
-                                    )
-                                  ) (to-regex rules-def (rest rules)))))))
+           rule-body (if (= rule 8) [[42]] rule-body)
+           rule-body (if (= rule 11) [[]] rule-body)
+           rule-regex (if (char? rule-body) (str rule-body)
+                                            (if (coll? (first rule-body))
+                                              (let [sub-rules (first rule-body)
+                                                    sub-rules2 (second rule-body)
+                                                    r1 (to-regex rules-def sub-rules)
+                                                    r2 (to-regex rules-def sub-rules2)
+                                                    r (if (empty? r2) r1 (str "(" r1 "|" r2 ")"))]
+                                                r)
+                                              (reduce (fn [acc other-rule] (str acc (to-regex rules-def [other-rule]))) "" rule-body)
+                                              )
+                                            )
+           rule-regex (if (= rule 8)
+                        (str "(" rule-regex ")+")
+                        rule-regex)
+           rule-regex (if (= rule 11)
+                        (let [rule42 (to-regex rules-def [42])
+                              rule31 (to-regex rules-def [31])]
+                          (str "(" (reduce (fn [acc val] (str  acc "|" "((" rule42 "){" val "}" "(" rule31 "){" val "})")  ) (str "((" rule42 "){1}" "(" rule31 "){1})") (range 2 10)) ")")
+                          )
+                        rule-regex)
+           result (str rule-regex (to-regex rules-def (rest rules)))]
+       result
+       ))))
 
 (deftest to-regex-test
   (testing "to-regex"
@@ -49,7 +62,8 @@
     (is (= "^a$" (to-regex {0 [[1] []], 1 \a, 2 \b})))
     (is (= "^a$" (to-regex {0 [[1]], 1 \a, 2 \b})))
     (is (= "^aa$" (to-regex {0 [[1 2]], 1 [[2]], 2 \a})))
-    (is (= "" (to-regex rules-def)))
+    (is (= "^aa$" (to-regex {0 [[1 2]], 1 [[2]], 2 \a})))
+    (is (= "^a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b$" (to-regex rules-def)))
     ))
 
 (deftest gen-test
@@ -86,30 +100,34 @@
     (is (= rules-def (parse-rule-def input)))
     ))
 
-;(defn solve-1
-;  [input]
-;  (let [[rules-def-input messages] (str/split input #"\n\n")
-;        rules-def (parse-rule-def rules-def-input)
-;        messages (str/split-lines messages)
-;        possible-matches (set (gen rules-def [0]))
-;        matched (map #(contains? possible-matches %) messages)
-;        ]
-;    (count (filter true? matched))
-;    ))
-
 (defn solve-1
   [input]
   (let [[rules-def-input messages] (str/split input #"\n\n")
         rules-def (parse-rule-def rules-def-input)
         messages (str/split-lines messages)
-        regex (re-pattern (to-regex rules-def))
-        matched (map #(re-matches regex %) messages)
-        ;_ (println matched)
+        possible-matches (set (gen rules-def [0]))
+        matched (map #(contains? possible-matches %) messages)
         ]
-    (count (remove nil? matched))
+    (count (filter true? matched))
+    ))
+
+(defn solve-2
+  [input]
+  (let [[rules-def-input messages] (str/split input #"\n\n")
+        rules-def (parse-rule-def rules-def-input)
+        messages (str/split-lines messages)
+        regex (re-pattern (to-regex rules-def))
+        matched (map #(not= nil (re-matches regex %)) messages)
+        ]
+    (count (remove false? matched))
     ))
 
 (deftest solve-1-test
   (testing "solve-1"
     (is (= 113 (solve-1 puzzle-input)))
+    ))
+
+(deftest solve-2-test
+  (testing "solve-2"
+    (is (= 253 (solve-2 puzzle-input)))
     ))
