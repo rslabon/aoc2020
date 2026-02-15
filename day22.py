ae94f855c798b9ac2fed8bf332d81d9943426cbd
abc <abc@abc.abc>
2026-02-15 14:24:07 +0100

    day 22 - done

--- day22.py
+++ day22.py
@@ -30,6 +30,16 @@ def parse_player_cards(data):
     return deque(player_cards)
 
 
+def compute_score(winning_cards):
+    m = 1
+    score = 0
+    for card in reversed(winning_cards):
+        score += card * m
+        m += 1
+
+    return score
+
+
 def part1():
     player1_cards = parse_player_cards(player1_data)
     player2_cards = parse_player_cards(player2_data)
@@ -37,34 +47,70 @@ def part1():
     round = 0
     while player1_cards and player2_cards:
         round += 1
-        print(f"-- Round {round} --")
-        print(f"Player 1's deck: {player1_cards}")
-        print(f"Player 2's deck: {player2_cards}")
         card1 = player1_cards.popleft()
         card2 = player2_cards.popleft()
-        print(f"Player 1 plays: {card1}")
-        print(f"Player 2 plays: {card2}")
         if card1 > card2:
-            print("Player 1 wins the round!")
             player1_cards.append(card1)
             player1_cards.append(card2)
         else:
-            print("Player 2 wins the round!")
             player2_cards.append(card2)
             player2_cards.append(card1)
 
-    print("== Post-game results ==")
-    print(f"Player 1's deck {player1_cards}")
-    print(f"Player 2's deck {player2_cards}")
+    score = compute_score(player1_cards + player2_cards)
+    print(score)
 
-    winning_cards = player1_cards + player2_cards
-    m = 1
-    score = 0
-    for card in reversed(winning_cards):
-        score += card * m
-        m += 1
 
+def play(player1_cards, player2_cards):
+    if not player1_cards:
+        return 2
+    if not player2_cards:
+        return 1
+
+    seen = set()
+    round = 0
+    while player1_cards and player2_cards:
+        cards1 = tuple(player1_cards)
+        cards2 = tuple(player2_cards)
+        if (cards1, cards2) in seen:
+            return 1
+
+        seen.add((cards1, cards2))
+
+        round += 1
+        card1 = player1_cards.popleft()
+        card2 = player2_cards.popleft()
+
+        if card1 <= len(player1_cards) and card2 <= len(player2_cards):
+            winner = play(deque(list(player1_cards)[0:card1]), deque(list(player2_cards)[0:card2]))
+            if winner == 1:
+                player1_cards.append(card1)
+                player1_cards.append(card2)
+            else:
+                player2_cards.append(card2)
+                player2_cards.append(card1)
+        else:
+            if card1 > card2:
+                player1_cards.append(card1)
+                player1_cards.append(card2)
+            else:
+                player2_cards.append(card2)
+                player2_cards.append(card1)
+
+    if player1_cards:
+        return 1
+    if player2_cards:
+        return 2
+
+    raise "ERROR!!!"
+
+
+def part2():
+    player1_cards = parse_player_cards(player1_data)
+    player2_cards = parse_player_cards(player2_data)
+    play(player1_cards, player2_cards)
+    score = compute_score(player1_cards + player2_cards)
     print(score)
 
 
 part1()
+part2()
